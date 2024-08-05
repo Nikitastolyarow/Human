23c36853bfafe3de1f5cd3aff9dedcc56bec5b1b
Nikitastolyarow <nikita.stolyarow2002@yandex.ru>
2024-08-05 22:12:07 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,80 @@
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String adress;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+            return age >= 0;
+    }
+
+    public boolean hasAddress() {
+            return adress != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+            return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return adress;
+    }
+
+    public void setAddress(String address) {
+            this.adress = address;
+    }
+
+    public void happyBirthday() {
+           if(hasAge())
+               age++;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(surname).setAddress(adress);
+    }
+
+    @Override
+    public String toString() {
+        return "Person" +
+                "\n name=" + name +
+                "\n surname=" + surname +
+                "\n age=" + age +
+                "\n adress=" + adress;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return age == person.age && name.equals(person.name) && surname.equals(person.surname)&&
+                Objects.equals(adress, person.adress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+}
